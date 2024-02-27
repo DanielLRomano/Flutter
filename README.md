dcb24a2b94532c1ed9575af49269afa0bcf55043
DanielLRomano <romanodaniel1012@gmail.com>
2024-02-27 20:06:52 -0300

    27/02

---
+++ README.md
@@ -0,0 +1,1 @@
+# Aprendizagem Senai - Desenvolvimento Aplicativo Mobile 
--- jogo_advinha_flutter/android/app/src/main/kotlin/com/example/jogo_advinha_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.jogo_advinha_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
