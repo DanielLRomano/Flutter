34a916bca0d7ba8c124afc8e6a54c0ff93554c71
DanielLRomano <romanodaniel1012@gmail.com>
2024-04-29 21:44:46 -0300

    29/04

---
+++ exemplo_json2/lib/Model/livros_model.dart
@@ -0,0 +1,37 @@
+class Livro {
+  //Atributos
+  final String titulo;
+  final String autor;
+  final String sinopse;
+  final String categoria;
+  final int isbn;
+
+  //Construtor
+  Livro(
+      {required this.titulo,
+      required this.autor,
+      required this.sinopse,
+      required this.categoria,
+      required this.isbn});
+
+  //Métodos (mapeamento)
+  Map<String, dynamic> toJson() {
+    return {
+      'titulo': titulo,
+      'autor': autor,
+      'sinopse': sinopse,
+      'categoria': categoria,
+      'isbn': isbn,
+    };
+  }
+
+  factory Livro.fromJson(Map<String, dynamic> map) {
+    return Livro(
+      titulo: map['titulo'],
+      autor: map['autor'],
+      sinopse: map['sinopse'],
+      categoria: map['categoria'],
+      isbn: map['isbn'],
+    );
+  }
+}
---
+++ exemplo_json2/lib/View/info_livro_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LivroInfoPage extends StatelessWidget {
+  const LivroInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Livro Information'),
+      ),
+      body: Center(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
